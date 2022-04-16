608a282fe06bafc28ba197033f40b0ef513930fe
Monster2408 <meoto2408@gmail.com>
2022-04-17 02:10:32 +0900

    add simple gui type

---
+++ main.py
@@ -0,0 +1,18 @@
+# !C:\Users\enjoy\AppData\Local\Programs\Python\Python38\python.exe
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+import PySimpleGUI as sg
+
+import Var
+import function as func
+
+def main():
+    func.makeResouceFiles()
+    
+
+
+if __name__ == '__main__':
+    icon = Var.BASE64_ICON
+    main()
